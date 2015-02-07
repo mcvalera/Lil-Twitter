07eb710b665ef235211ffa05d0c31a04c37f3f7c
lucasosouza & Clarisse Valera <pair+lucasosouza.mcvalera@devbootcamp.com>
2015-02-06 16:12:36 -0800

    added followers relationshop

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,6 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :follower, foreign_key: "follower_id", class_name: "User"
-  belongs_to :followee, foreign_key: "followee_id", class_name: "User"
+  belongs_to :fan, foreign_key: "fan_id", class_name: "User"
+  belongs_to :idol, foreign_key: "idol_id", class_name: "User"
 
+  # Remember to create a migration!
 end
